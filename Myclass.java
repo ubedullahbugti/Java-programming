efbecb53e95e46854c70723fd54702ae7ebbd523
ubedullahbugti <115548581+ubedullahbugti@users.noreply.github.com>
2022-10-15 07:44:52 +0500

    Add files via upload

---
+++ Myclass.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Myclass{
+public static void main(String[] org)
+{
+int x=10;
+int y=20;
+System.out.println(x);
+System.out.println(y);
+
+double p=30.8;
+double q=70.6;
+
+System.out.println(p);
+System.out.println(q);
+
+char a='a';
+char b='b';
+System.out.println(a);
+System.out.println(b);
+
+
+
+}
+}
