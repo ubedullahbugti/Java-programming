03a64d93b96f44e869ec726c5189ee29e5269e5b
ubedullahbugti <115548581+ubedullahbugti@users.noreply.github.com>
2022-10-15 07:44:04 +0500

    Add files via upload

---
+++ op.java
@@ -0,0 +1,16 @@
+class op{
+public static void main(String[] or){
+int x=4;
+int y=2;
+System.out.println("The addition of two numbers is "+ (x+y));
+
+System.out.println("The Subtraction of two numbers is "+ (x-y));
+
+System.out.println("The Multiplication of two numbers is "+ (x*y));
+
+System.out.println("The Division of two numbers is "+ (x/y));
+
+System.out.println("The Rmainder of two numbers is "+ (x%y));
+}
+}
+
